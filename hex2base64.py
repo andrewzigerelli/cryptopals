55519e8d3150bab33b21060f26e4427db6e470db
andrewzigerelli <andrewdzigerelli@users.noreply.github.com>
2017-12-26 14:52:48 -0500

    updated for python3 and returned string

--- hex2base64.py
+++ hex2base64.py
@@ -1,15 +1,16 @@
-#!/usr/bin/python
+#!/usr/bin/python3
 import sys
 from base64 import b64encode
 from binascii import unhexlify
 
 def hex2base64( hexstr ):
-    "This function converts input hex to base64"
+    "This function converts input hex string to base64"
+    "It returns a string of the base64 representation"
     byte = unhexlify(hexstr)
-    base64 = b64encode(byte)
+    base64 = b64encode(byte).decode()
     return base64;
 
 #main
 hexstr = sys.argv[1]
 base64 = hex2base64(hexstr)
-print base64
+print(base64)
