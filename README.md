b39110f37d67e7624dfd6ced75132baf77456952
Andrew Zigerelli <andrewzigerelli@users.noreply.github.com>
2018-01-02 22:25:45 -0500

    updated readme, singlebytexor updated to be callable from other functions

--- README.md
+++ README.md
@@ -3,3 +3,8 @@ the cryptopals challenges
 
 i'm going to attempt to do this in python so I "get it" and finally become one
 of those millions who love python
+
+cryptopals recommends working on 'raw bytes', but python's bytes type sucks. you
+can't even xor two bytes together. so, i instead work on strings, and I convert
+an "ascii string" to a "hex string" using functions in helper.py. "hex strings"
+are my raw bytes.
--- singlebytexor.py
+++ singlebytexor.py
@@ -7,6 +7,15 @@ from helper import *
 #main
 def main():
     enc = '1b37373331363f78151b7f2b783431333d78397828372d363c78373e783a393b3736'
+    msg = singlebytexor(enc)
+    print(msg)
+
+
+def singlebytexor(enc, freq_guess=" "):
+    """decrypts the string enc, assuming that it's encrypted with
+    enc=xor(msg,key), where the key is the same length of msg and is a
+    repetition of a single byte. Uses freq as the guess for most frequent char
+    in the ciphertext; defaults to space."""
     freq = freq_score(enc)
     freq_char = freq[0][1]
     #print(freq_char)
@@ -14,14 +23,14 @@ def main():
 
     #assume this freq_char maps to space
     #http://fitaly.com/board/domper3/posts/136.html
-    space = ascii2hex(" ")
-    key_char = xor(space, freq_char)
+    freq_guess = ascii2hex(freq_guess)
+    key_char = xor(freq_guess, freq_char)
     #create full length key and decrypt
     key = key_char * int(len(enc) / 2)
 
     msg = xor(key, enc)
     msg = hex2ascii(msg)
-    print(msg)
+    return msg
 
 
 def freq_score(string):
